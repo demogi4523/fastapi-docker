34b27e589c174b063a866ef08c6784e1b56d90ce
demogi4523 <demogi4523@yandex.ru>
2025-04-29 03:34:11 +0000

    autorun app from main module

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'key' : 'value'}
+
+
+if __name__ == '__main__':
+    import asyncio
+    from hypercorn.config import Config
+    from hypercorn.asyncio import serve
+
+    port = 8001
+    bind = [f"0.0.0.0:{port}"]
+    config = Config()
+    config.bind = bind
+    asyncio.run(serve(app, config))
